a73acfd894786b80331e1bae8b60271b887ca48b
samtheerapong <theerapong.khan@gmail.com>
2023-06-22 09:01:37 +0700

    commit

---
+++ application/views/category/index.php
@@ -0,0 +1,370 @@
+
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+    ระบบจัดการ
+      <small>กลุ่มอุปกรณ์ / อะไหล่</small>
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> หน้าหลัก</a></li>
+      <li class="active">กลุ่มอุปกรณ์ / อะไหล่</li>
+    </ol>
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <!-- Small boxes (Stat box) -->
+    <div class="row">
+      <div class="col-md-12 col-xs-12">
+
+        <div id="messages"></div>
+
+        <?php if($this->session->flashdata('success')): ?>
+          <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <?php echo $this->session->flashdata('success'); ?>
+          </div>
+        <?php elseif($this->session->flashdata('error')): ?>
+          <div class="alert alert-error alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <?php echo $this->session->flashdata('error'); ?>
+          </div>
+        <?php endif; ?>
+
+        <?php if(in_array('createCategory', $user_permission)): ?>
+          <button class="btn btn-primary" data-toggle="modal" data-target="#addModal"><i class="fa fa-plus"></i> เพิ่มข้อมูล</button>
+          <br /> <br />
+        <?php endif; ?>
+
+        <div class="box">
+          <div class="box-header">
+            <h3 class="box-title">กลุ่มอุปกรณ์ / อะไหล่</h3>
+          </div>
+          <!-- /.box-header -->
+          <div class="box-body">
+            <table id="manageTable" class="table table-bordered table-striped">
+              <thead>
+              <tr>
+                <th>กลุ่มอุปกรณ์ / อะไหล่</th>
+                <th>สถานะ</th>
+                <?php if(in_array('updateCategory', $user_permission) || in_array('deleteCategory', $user_permission)): ?>
+                  <th>จัดการ</th>
+                <?php endif; ?>
+              </tr>
+              </thead>
+
+            </table>
+          </div>
+          <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+      </div>
+      <!-- col-md-12 -->
+    </div>
+    <!-- /.row -->
+    
+
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php if(in_array('createCategory', $user_permission)): ?>
+<!-- create brand modal -->
+<div class="modal fade" tabindex="-1" role="dialog" id="addModal">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">เพิ่มข้อมูล</h4>
+      </div>
+
+      <form role="form" action="<?php echo base_url('category/create') ?>" method="post" id="createForm">
+
+        <div class="modal-body">
+
+          <div class="form-group">
+            <label for="brand_name">กลุ่มอุปกรณ์ / อะไหล่</label>
+            <input type="text" class="form-control" id="category_name" name="category_name" placeholder="" autocomplete="off">
+          </div>
+          <div class="form-group">
+            <label for="active">สถานะ</label>
+            <select class="form-control" id="active" name="active">
+              <option value="1">ใช้งาน</option>
+              <option value="2">ไม่ใช้งาน</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">ปิด</button>
+          <button type="submit" class="btn btn-primary">บันทึก</button>
+        </div>
+
+      </form>
+
+
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<?php endif; ?>
+
+<?php if(in_array('updateCategory', $user_permission)): ?>
+<!-- edit brand modal -->
+<div class="modal fade" tabindex="-1" role="dialog" id="editModal">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">แก้ไขข้อมูล</h4>
+      </div>
+
+      <form role="form" action="<?php echo base_url('category/update') ?>" method="post" id="updateForm">
+
+        <div class="modal-body">
+          <div id="messages"></div>
+
+          <div class="form-group">
+            <label for="edit_brand_name">กลุ่มอุปกรณ์ / อะไหล่</label>
+            <input type="text" class="form-control" id="edit_category_name" name="edit_category_name" placeholder="" autocomplete="off">
+          </div>
+          <div class="form-group">
+            <label for="edit_active">สถานะ</label>
+            <select class="form-control" id="edit_active" name="edit_active">
+              <option value="1">ใช้งาน</option>
+              <option value="2">ไม่ใช้งาน</option>
+            </select>
+          </div>
+        </div>
+
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal"> <i class="fa fa-close"></i>  ปิด</button>
+          <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i>  บันทึก</button>
+        </div>
+
+      </form>
+
+
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<?php endif; ?>
+
+<?php if(in_array('deleteCategory', $user_permission)): ?>
+<!-- remove brand modal -->
+<div class="modal fade" tabindex="-1" role="dialog" id="removeModal">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">ลบข้อมูล</h4>
+      </div>
+
+      <form role="form" action="<?php echo base_url('category/remove') ?>" method="post" id="removeForm">
+        <div class="modal-body">
+          <p>ต้องการลบข้อมูลหรือไม่?</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" data-dismiss="modal"> <i class="fa fa-close"></i> ปิด</button>
+          <button type="submit" class="btn btn-danger"> <i class="fa fa-trash"></i> ลบ</button>
+        </div>
+      </form>
+
+
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<?php endif; ?>
+
+
+<script type="text/javascript">
+var manageTable;
+
+$(document).ready(function() {
+  $("#categoryNav").addClass('active');
+  
+  // initialize the datatable 
+  manageTable = $('#manageTable').DataTable({
+    'ajax': 'fetchCategoryData',
+    'order': []
+  });
+
+  // submit the create from 
+  $("#createForm").unbind('submit').on('submit', function() {
+    var form = $(this);
+
+    // remove the text-danger
+    $(".text-danger").remove();
+
+    $.ajax({
+      url: form.attr('action'),
+      type: form.attr('method'),
+      data: form.serialize(), // /converting the form data into array and sending it to server
+      dataType: 'json',
+      success:function(response) {
+
+        manageTable.ajax.reload(null, false); 
+
+        if(response.success === true) {
+          $("#messages").html('<div class="alert alert-success alert-dismissible" role="alert">'+
+            '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+            '<strong> <span class="glyphicon glyphicon-ok-sign"></span> </strong>'+response.messages+
+          '</div>');
+
+
+          // hide the modal
+          $("#addModal").modal('hide');
+
+          // reset the form
+          $("#createForm")[0].reset();
+          $("#createForm .form-group").removeClass('has-error').removeClass('has-success');
+
+        } else {
+
+          if(response.messages instanceof Object) {
+            $.each(response.messages, function(index, value) {
+              var id = $("#"+index);
+
+              id.closest('.form-group')
+              .removeClass('has-error')
+              .removeClass('has-success')
+              .addClass(value.length > 0 ? 'has-error' : 'has-success');
+              
+              id.after(value);
+
+            });
+          } else {
+            $("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">'+
+              '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+              '<strong> <span class="glyphicon glyphicon-exclamation-sign"></span> </strong>'+response.messages+
+            '</div>');
+          }
+        }
+      }
+    }); 
+
+    return false;
+  });
+
+});
+
+// edit function
+function editFunc(id)
+{ 
+  $.ajax({
+    url: 'fetchCategoryDataById/'+id,
+    type: 'post',
+    dataType: 'json',
+    success:function(response) {
+
+      $("#edit_category_name").val(response.name);
+      $("#edit_active").val(response.active);
+
+      // submit the edit from 
+      $("#updateForm").unbind('submit').bind('submit', function() {
+        var form = $(this);
+
+        // remove the text-danger
+        $(".text-danger").remove();
+
+        $.ajax({
+          url: form.attr('action') + '/' + id,
+          type: form.attr('method'),
+          data: form.serialize(), // /converting the form data into array and sending it to server
+          dataType: 'json',
+          success:function(response) {
+
+            manageTable.ajax.reload(null, false); 
+
+            if(response.success === true) {
+              $("#messages").html('<div class="alert alert-success alert-dismissible" role="alert">'+
+                '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+                '<strong> <span class="glyphicon glyphicon-ok-sign"></span> </strong>'+response.messages+
+              '</div>');
+
+
+              // hide the modal
+              $("#editModal").modal('hide');
+              // reset the form 
+              $("#updateForm .form-group").removeClass('has-error').removeClass('has-success');
+
+            } else {
+
+              if(response.messages instanceof Object) {
+                $.each(response.messages, function(index, value) {
+                  var id = $("#"+index);
+
+                  id.closest('.form-group')
+                  .removeClass('has-error')
+                  .removeClass('has-success')
+                  .addClass(value.length > 0 ? 'has-error' : 'has-success');
+                  
+                  id.after(value);
+
+                });
+              } else {
+                $("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">'+
+                  '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+                  '<strong> <span class="glyphicon glyphicon-exclamation-sign"></span> </strong>'+response.messages+
+                '</div>');
+              }
+            }
+          }
+        }); 
+
+        return false;
+      });
+
+    }
+  });
+}
+
+// remove functions 
+function removeFunc(id)
+{
+  if(id) {
+    $("#removeForm").on('submit', function() {
+
+      var form = $(this);
+
+      // remove the text-danger
+      $(".text-danger").remove();
+
+      $.ajax({
+        url: form.attr('action'),
+        type: form.attr('method'),
+        data: { category_id:id }, 
+        dataType: 'json',
+        success:function(response) {
+
+          manageTable.ajax.reload(null, false); 
+
+          if(response.success === true) {
+            $("#messages").html('<div class="alert alert-success alert-dismissible" role="alert">'+
+              '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+              '<strong> <span class="glyphicon glyphicon-ok-sign"></span> </strong>'+response.messages+
+            '</div>');
+
+            // hide the modal
+            $("#removeModal").modal('hide');
+
+          } else {
+
+            $("#messages").html('<div class="alert alert-warning alert-dismissible" role="alert">'+
+              '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'+
+              '<strong> <span class="glyphicon glyphicon-exclamation-sign"></span> </strong>'+response.messages+
+            '</div>'); 
+          }
+        }
+      }); 
+
+      return false;
+    });
+  }
+}
+
+
+</script>
---
+++ application/views/products/create.php
@@ -0,0 +1,185 @@
+
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      ระบบจัดการ
+      <small>อะไหล่</small>
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> หน้าหลัก</a></li>
+      <li class="active">อะไหล่</li>
+    </ol>
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+    <!-- Small boxes (Stat box) -->
+    <div class="row">
+      <div class="col-md-12 col-xs-12">
+
+        <div id="messages"></div>
+
+        <?php if($this->session->flashdata('success')): ?>
+          <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <?php echo $this->session->flashdata('success'); ?>
+          </div>
+        <?php elseif($this->session->flashdata('error')): ?>
+          <div class="alert alert-error alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <?php echo $this->session->flashdata('error'); ?>
+          </div>
+        <?php endif; ?>
+
+
+        <div class="box">
+          <div class="box-header">
+            <h3 class="box-title">เพิ่มอะไหล่</h3>
+          </div>
+          <!-- /.box-header -->
+          <form role="form" action="<?php base_url('users/create') ?>" method="post" enctype="multipart/form-data">
+              <div class="box-body">
+
+                <?php echo validation_errors(); ?>
+
+                <div class="form-group">
+
+                  <label for="product_image">รูปภาพ</label>
+                  <div class="kv-avatar">
+                      <div class="file-loading">
+                          <input id="product_image" name="product_image" type="file">
+                      </div>
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label for="product_name">รายการอะไหล่</label>
+                  <input type="text" class="form-control" id="product_name" name="product_name" placeholder="" autocomplete="off"/>
+                </div>
+
+                <div class="form-group">
+                  <label for="sku">รหัส</label>
+                  <input type="text" class="form-control" id="sku" name="sku" placeholder="" autocomplete="off" />
+                </div>
+
+                <div class="form-group">
+                  <label for="price">ราคา</label>
+                  <input type="text" class="form-control" id="price" name="price" placeholder="" autocomplete="off" />
+                </div>
+
+                <div class="form-group">
+                  <label for="qty">จำนวน</label>
+                  <input type="text" class="form-control" id="qty" name="qty" placeholder="" autocomplete="off" />
+                </div>
+
+                <div class="form-group">
+                  <label for="description">รายละเอียด</label>
+                  <textarea type="text" class="form-control" id="description" name="description" placeholder="" autocomplete="off">
+                  </textarea>
+                </div>
+
+                <?php if($attributes): ?>
+                  <?php foreach ($attributes as $k => $v): ?>
+                    <div class="form-group">
+                      <label for="groups"><?php echo $v['attribute_data']['name'] ?></label>
+                      <select class="form-control select_group" id="attributes_value_id" name="attributes_value_id[]" multiple="multiple">
+                        <?php foreach ($v['attribute_value'] as $k2 => $v2): ?>
+                          <option value="<?php echo $v2['id'] ?>"><?php echo $v2['value'] ?></option>
+                        <?php endforeach ?>
+                      </select>
+                    </div>    
+                  <?php endforeach ?>
+                <?php endif; ?>
+
+                <div class="form-group">
+                  <label for="brands">ลักษณะ</label>
+                  <select class="form-control select_group" id="brands" name="brands[]" multiple="multiple">
+                    <?php foreach ($brands as $k => $v): ?>
+                      <option value="<?php echo $v['id'] ?>"><?php echo $v['name'] ?></option>
+                    <?php endforeach ?>
+                  </select>
+                </div>
+
+                <div class="form-group">
+                  <label for="category">ประเภท</label>
+                  <select class="form-control select_group" id="category" name="category[]" multiple="multiple">
+                    <?php foreach ($category as $k => $v): ?>
+                      <option value="<?php echo $v['id'] ?>"><?php echo $v['name'] ?></option>
+                    <?php endforeach ?>
+                  </select>
+                </div>
+
+                <div class="form-group">
+                  <label for="store">คลังเก็บ</label>
+                  <select class="form-control select_group" id="store" name="store">
+                    <?php foreach ($stores as $k => $v): ?>
+                      <option value="<?php echo $v['id'] ?>"><?php echo $v['name'] ?></option>
+                    <?php endforeach ?>
+                  </select>
+                </div>
+
+                <div class="form-group">
+                  <label for="store">การใช้งาน</label>
+                  <select class="form-control" id="availability" name="availability">
+                    <option value="1">ใช้งาน</option>
+                    <option value="2">ไม่ใช้งาน</option>
+                  </select>
+                </div>
+
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary"> <i class="fa fa-save"></i> บันทึก</button>
+                <a href="<?php echo base_url('products/') ?>" class="btn btn-warning"> <i class="fa fa-mail-reply"></i> ย้อนกลับ</a>
+              </div>
+            </form>
+          <!-- /.box-body -->
+        </div>
+        <!-- /.box -->
+      </div>
+      <!-- col-md-12 -->
+    </div>
+    <!-- /.row -->
+    
+
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $(".select_group").select2();
+    $("#description").wysihtml5();
+
+    $("#mainProductNav").addClass('active');
+    $("#addProductNav").addClass('active');
+    
+    var btnCust = 
+        '<button type="button" class="btn btn-secondary" title="Add picture tags" ' + 
+        'onclick="alert(\'ระบบจะสร้างแท็กอัตโนมัติ\')">' +
+        '<i class="glyphicon glyphicon-tag"></i>' +
+        '</button>'; 
+    $("#product_image").fileinput({
+        overwriteInitial: true,
+        maxFileSize: 1500,
+        showClose: false,
+        showCaption: false,
+        browseLabel: '',
+        removeLabel: '',
+        browseIcon: '<i class="glyphicon glyphicon-folder-open"></i>',
+        removeIcon: '<i class="glyphicon glyphicon-remove"></i>',
+        removeTitle: 'Cancel or reset changes',
+        elErrorContainer: '#kv-avatar-errors-1',
+        msgErrorClass: 'alert alert-block alert-danger',
+        // defaultPreviewContent: '<img src="/uploads/default_avatar_male.jpg" alt="Your Avatar">',
+        layoutTemplates: {main2: '{preview} ' +  btnCust + ' {remove} {browse}'},
+        allowedFileExtensions: ["jpg", "png", "gif"]
+    });
+
+  });
+</script>
