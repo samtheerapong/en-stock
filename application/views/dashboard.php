0306decd0b2a0324ff1e566ec4c4280fd597aa30
samtheerapong <theerapong.khan@gmail.com>
2023-06-22 16:11:47 +0700

    up

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -4,7 +4,7 @@
     <section class="content-header">
         <h1>
         แผงควบคุม
-            <small>NFC EN Stock</small>
+            <small>NFC Engineering Stock</small>
         </h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> หน้าหลัก</a></li>
@@ -15,8 +15,7 @@
     <!-- Main content -->
     <section class="content">
         <!-- Small boxes (Stat box) -->
-        
-        <?php if($is_admin == 'true'): ?>
+        <?php if($is_admin == true): ?>
 
         <div class="row">
             <div class="col-lg-3 col-xs-6">
@@ -35,7 +34,22 @@
                 </div>
             </div>
             <!-- ./col -->
-           
+            <div class="col-lg-3 col-xs-6">
+                <!-- small box -->
+                <div class="small-box bg-green">
+                    <div class="inner">
+                        <h3><?php echo $total_paid_orders ?></h3>
+
+                        <p>รายการเบิก</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="<?php echo base_url('orders/') ?>" class="small-box-footer">More info <i
+                            class="fa fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
             <div class="col-lg-3 col-xs-6">
                 <!-- small box -->
                 <div class="small-box bg-yellow">
